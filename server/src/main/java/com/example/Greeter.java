afcf72718083aff44784df212e398b08422acccb
Shreyanshsg788 <30348534+Shreyanshsg788@users.noreply.github.com>
2019-04-06 10:45:52 +0530

    commenting_javadoc_comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,8 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  
+  /** {@inheritDoc} */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
